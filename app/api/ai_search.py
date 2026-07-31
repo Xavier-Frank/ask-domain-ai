46e2a664fdefba0ad53739ca33e83050f6f82cde
Xavier-Frank <oduorfrancis134@gmail.com>
2026-07-31 16:03:12 +0300

    updated route

--- app/api/ai_search.py
+++ app/api/ai_search.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from app.llm_services.ollama_service import OllamaService
 from app.models.search.search_request import SearchRequest
 
-router = APIRouter(prefix="/ai-search", tags=["Search"])
+router = APIRouter(prefix="/search", tags=["Search"])
 
 @router.post("/ai-query", tags=["Search"])
 def search(request: SearchRequest):
